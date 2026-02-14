66f28394e4bd0f178734fc64737ff7ff3ef6b3cb
gdkwkwjm2014-afk <glebsmek2014@gmail.com>
2026-02-14 07:12:19 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,14 +1,6 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := DarknessVip
-
-# Мы оставляем только два файла, чтобы Гитхаб не сходил с ума
-# Это гарантирует, что компиляция пройдет быстро и без ошибок
-LOCAL_SRC_FILES := Main.cpp \
-                   Memory.cpp
-
+LOCAL_SRC_FILES := Main.cpp Memory.cpp
 LOCAL_LDLIBS := -llog -landroid
-
 include $(BUILD_SHARED_LIBRARY)
