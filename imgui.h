b6a46a4e2b0b230cb5702107d6aad0671163f8a8
gdkwkwjm2014-afk <glebsmek2014@gmail.com>
2026-02-14 06:26:18 +0200

    Update imgui.h

--- imgui.h
+++ imgui.h
@@ -4,10 +4,20 @@
 
 #define IMGUI_API
 typedef unsigned int ImGuiID;
+typedef unsigned long long ImU64;
 typedef char ImWchar;
 
-struct ImVec2 { float x, y; ImVec2() { x = y = 0.0f; } ImVec2(float _x, float _y) { x = _x; y = _y; } };
-struct ImVec4 { float x, y, z, w; };
+struct ImVec2 { 
+    float x, y; 
+    ImVec2() { x = y = 0.0f; } 
+    ImVec2(float _x, float _y) { x = _x; y = _y; } 
+};
+
+struct ImVec4 { 
+    float x, y, z, w; 
+    ImVec4() { x = y = z = w = 0.0f; } 
+    ImVec4(float _x, float _y, float _z, float _w) { x = _x; y = _y; z = _z; w = _w; } 
+};
 
 namespace ImGui {
     IMGUI_API bool Begin(const char* name, bool* p_open = NULL, int flags = 0);
