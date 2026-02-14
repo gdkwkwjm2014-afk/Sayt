a93744b181d47063ee2cc9d0da1122805edb1749
gdkwkwjm2014-afk <glebsmek2014@gmail.com>
2026-02-14 07:18:09 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,6 +1,17 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := DarknessVip
-LOCAL_SRC_FILES := Main.cpp Memory.cpp
+
+# Добавляем файлы движка, чтобы ушли ошибки "undefined reference"
+LOCAL_SRC_FILES := Main.cpp \
+                   Memory.cpp \
+                   imgui.cpp \
+                   imgui_draw.cpp \
+                   imgui_widgets.cpp \
+                   imgui_tables.cpp
+
 LOCAL_LDLIBS := -llog -landroid
+
 include $(BUILD_SHARED_LIBRARY)
