e4f448a9eb68c75067db5815abd7613a86fe02bd
gdkwkwjm2014-afk <glebsmek2014@gmail.com>
2026-02-14 06:16:58 +0200

    Update Memory.h

--- Memory.h
+++ Memory.h
@@ -3,31 +3,12 @@
 
 #include <stdint.h>
 #include <unistd.h>
-#include <string>
 
 class Memory {
 public:
-    // На Internal нам не нужен PID, мы уже в процессе
-    uintptr_t base_address = 0;
-
-    // Получаем адрес библиотеки игры (например, libUE4.so)
-    uintptr_t get_module_base(const char* module_name);
-
-    // Самое простое чтение в мире для Internal
-    template <typename T>
-    T read(uintptr_t address) {
-        if (address < 0xFFFFFF) return T(); // Защита от краша
-        return *(T*)address;
-    }
-
-    // Запись
-    template <typename T>
-    void write(uintptr_t address, T value) {
-        if (address < 0xFFFFFF) return;
-        *(T*)address = value;
-    }
+    int pid; // Добавили переменную, на которую ругается робот
+    int get_pid(const char* package_name);
+    bool read(long address, void* buffer, size_t size);
 };
-
-extern Memory mem;
 
 #endif
