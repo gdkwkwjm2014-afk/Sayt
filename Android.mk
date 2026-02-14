ad634cdc95f8416bd82858b060d32190eb77b8d6
gdkwkwjm2014-afk <glebsmek2014@gmail.com>
2026-02-14 08:13:45 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,6 +1,14 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
-LOCAL_MODULE := DarknessVip
-LOCAL_SRC_FILES := Main.cpp
-LOCAL_LDLIBS := -llog -landroid
+
+# Называем файл как у донора для маскировки
+LOCAL_MODULE := TDataMaster
+
+# Указываем только те файлы, которые у нас есть
+LOCAL_SRC_FILES := Main.cpp Memory.cpp
+
+# Добавляем стандартные библиотеки логов и графики
+LOCAL_LDLIBS := -llog -landroid -lGLESv2
+
 include $(BUILD_SHARED_LIBRARY)
