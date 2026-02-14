e86e8d257a68cee807ee591212ec3a45e9506bbb
gdkwkwjm2014-afk <glebsmek2014@gmail.com>
2026-02-14 07:52:38 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := DarknessVip
-LOCAL_SRC_FILES := Main.cpp Memory.cpp
+LOCAL_SRC_FILES := Main.cpp
 LOCAL_LDLIBS := -llog -landroid
 include $(BUILD_SHARED_LIBRARY)
